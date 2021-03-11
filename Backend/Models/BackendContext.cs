ce68b39f3436ea8565a9f55ce0fa06dfecc02d3a
Bruno Silva <brunpersilva@gmail.com>
2021-03-11 17:37:17 -0300

    Add project files.

---
+++ Backend/Models/BackendContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class BackendContext : DbContext
+    {
+        public BackendContext(DbContextOptions<BackendContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
